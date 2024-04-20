602f58d092e58fddaa04b36f778a00fffbf8e8de
James Lovallo <james.lovallo@thrivent.com>
2024-04-20 09:49:12 -0400

    .

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -13,10 +13,10 @@ export default ({
 	<!DOCTYPE html>
 	<html lang="en">
 		<head>
-			<meta charset="UTF-8" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<!-- meta -->
 			<title>${title}</title>
+			<meta charset="UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<meta name="description" content="${description}" />
 			<meta property="og:title" content="${title}" />
 			<meta property="og:description" content="${description}" />
